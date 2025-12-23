e52d25aec480b8db6131ee9f254ca68d1c6fcc3d
NARNIX0 <abdurrahmanalsharefnu@gmail.com>
2025-12-23 00:21:09 +0000

    compressed hero banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div 
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: 'url(/ramen.jpeg)',
+          backgroundImage: 'url(/hero-banner.webp)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
